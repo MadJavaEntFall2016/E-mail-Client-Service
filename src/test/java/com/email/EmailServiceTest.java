124bfc38af91be7d3a4cca1ec3727395bfd930e3
SSAJJALA <SSAJJALA@madisoncollege.edu>
2016-11-08 16:46:34 -0600

    updated

---
+++ src/test/java/com/email/EmailServiceTest.java
@@ -0,0 +1,31 @@
+package com.email;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by student on 11/8/16.
+ */
+public class EmailServiceTest {
+    private final Logger logger = Logger.getLogger(this.getClass());
+    EmailService emailService = null;
+
+    @Before
+    public void setUp() {
+        Email email = new Email("siva.sajjala@gmail.com","siva.sajjala@gmail.com","test", "test email");
+
+        emailService = new EmailService(email);
+    }
+
+    @Test
+    public void sendMail() throws Exception {
+        Response response = emailService.sendMail();
+        assertNotNull("unable to generate html content", Response);
+    }
+
+}
