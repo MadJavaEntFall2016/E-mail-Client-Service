e4408898ac8636e8e6f9a5e3acb03e7772666b81
SSAJJALA <SSAJJALA@madisoncollege.edu>
2016-11-16 10:48:04 -0600

    updated

--- src/main/java/com/email/webservice/Email.java
+++ src/main/java/com/email/webservice/Email.java
@@ -6,6 +6,8 @@ import org.apache.log4j.Logger;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  * Created by Siva Sajjala on 10/27/16.
@@ -33,7 +35,20 @@ public class Email {
                                 @PathParam("to") String toAddress,
                                 @PathParam("from") String fromAddress) {
 
-        com.email.Email email = new com.email.Email(toAddress,fromAddress,subject,messageBody);
+        String sub = "";
+        String body = "";
+        String fromemail = "";
+        String toemail = "";
+        try {
+            sub = URLDecoder.decode(subject, "UTF-8");
+            body = URLDecoder.decode(messageBody, "UTF-8");
+            fromemail = URLDecoder.decode(fromAddress, "UTF-8");
+            toemail = URLDecoder.decode(toAddress, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        com.email.Email email = new com.email.Email(toemail,fromemail,sub,body);
 
         EmailService service = new EmailService(email);
         Response response = service.sendMail();
